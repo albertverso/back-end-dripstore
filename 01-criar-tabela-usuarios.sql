e5c9371844b9bb0cf66f2a86199504ce413a60ec
albertverso <caralbertverso@gmail.com>
2024-07-31 18:05:42 -0300

    fix

--- 01-criar-tabela-usuarios.sql
+++
@@ -1,8 +0,0 @@
--- Criar a tabela de usuários 
-CREATE TABLE `digitalcollege`.`usuarios` (
-  `id` INT NOT NULL,
-  `firstname` VARCHAR(45) NULL,
-  `surname` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `password` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
