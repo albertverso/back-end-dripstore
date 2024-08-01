4f0dab01a5391599e93071762d3c471bf26110ba
Diego Araujo <ferreiradearaujod@gmail.com>
2024-07-31 23:52:16 -0300

    Atualizando chaves estrangeiras e foreign key

--- 04-tabela-produtos-images.sql
+++ 04-tabela-produtos-images.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `dc.produtos`.`produtos_imagem` (
   `id` INT NOT NULL,
   `product_id` INT NULL,
-  `enabled` TINYINT NULL,
-  `path` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+  `enabled` BOOLEAN NULL,
+  `path` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`)
+  FOREIGN KEY (`id`) REFERENCES `dc.produtos`.`produtos_imagem` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  );
