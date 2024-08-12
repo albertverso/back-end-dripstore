bfe6b3f532c4fc9c97ca70591d395dcc97a1f3f8
albertverso <caralbertverso@gmail.com>
2024-08-12 20:41:50 -0300

    fix endpoint login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const { login } = require('../controllers/authController');
 const {authenticateToken} = require('../middleware/authMiddleware')
 
 // Endpoint de login
-app.post('/v1/user/token', login);
+app.post('/v1/user/login', login);
 
 app.get('/v1/user/meus-pedidos', authenticateToken , (req, res) => {
     res.json({ message: 'Acesso permitido', user: req.user })});
