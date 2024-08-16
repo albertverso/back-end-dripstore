57952a92369d77119b6570737e530240f82220ce
Gabriel Henrique <henriquedeveloper24@gmail.com>
2024-08-16 20:55:35 -0300

    fix: conexão com supabase feita com sucesso

--- teste.js
+++ teste.js
@@ -3,8 +3,6 @@ const {Sequelize } =  require("sequelize")
 
 const sequelize = new Sequelize('postgresql://postgres.ehtducsjvbqibjyhjrkp:projeto-backend@aws-0-sa-east-1.pooler.supabase.com:6543/postgres', {
     dialect : "postgresql",
-    host: process.env.HOST,
-    logging: console.log,
 })
 
 async function testConnection() {
