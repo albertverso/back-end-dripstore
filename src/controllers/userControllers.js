3e4fa5a24baac6442f355d74459d5c0c9f36db57
albertverso <caralbertverso@gmail.com>
2024-08-16 21:52:48 -0300

    fix: tratando erros nos endpoints

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -2,29 +2,49 @@ const {User} = require('../models/user')
 
 // Adicionar um novo usuário
 const CreateUser = async (req, res) => {
-    User.create(req.body).then((result) => res.status(201).send(result))
+    try {
+        User.create(req.body).then((result) => res.status(201).send(result))
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao criar usuário' });
+    }
   };
   
 // Buscar todos os usuários
 const SearchUserAll = async (req, res) => {
-    User.findAll().then((result) => res.send(result))
+    try {
+        User.findAll().then((result) => res.send(result))
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar todos os usuários' });
+    }
 };
 
 // Buscar um usuário por ID
 const SearchUserId = async (req, res) => {
-    User.findOne({ where: { id: req.params.id } }).then((result) => res.send(result))
+    try{
+        User.findOne({ where: { id: req.params.id } }).then((result) => res.send(result))
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao buscar usuário' });
+    }
 };
 
 // Atualizar um usuário por ID
 const UpdateUser = async (req, res) => {
-    User.update(req.body, { where: { id: req.params.id } }).then((result) => res.send(result))
+    try {
+        User.update(req.body, { where: { id: req.params.id } }).then((result) => res.send(result))
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao atualizar usuário' });
+    }
 };
 
 // Deletar um usuário por ID
 const DeleteUser = async (req, res) => {
-    User.destroy({ where: { id: req.params.id } }).then((result) => {
-        res.send('deletei com sucesso essa quantidade de linhas: '+result)
-    })
+    try {
+        User.destroy({ where: { id: req.params.id } }).then((result) => {
+            res.send('deletei com sucesso essa quantidade de linhas: '+result)
+        })
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao deletar usuário' });
+    }
 };
 
 module.exports = {
